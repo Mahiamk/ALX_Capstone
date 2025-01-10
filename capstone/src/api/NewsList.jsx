431d660841d4f22067de76efcd757ea0d6f050d0
Mahiamk <mahikomohammed@gmail.com>
2025-01-10 18:21:59 +0800

    feat: Add NewsList component to display a grid of news articles

--- capstone/src/api/NewsList.jsx
+++ capstone/src/api/NewsList.jsx
@@ -3,7 +3,7 @@ import NewsCard from './NewsCard';
 import './NewsList.css'; // Import the CSS file
 
 
-const NewsList = ({ articles }) => {
+const NewsList = ({ articles }) => {  
   return (
     <div className="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-7 gap-9">
       {articles.map((article, index) => (
